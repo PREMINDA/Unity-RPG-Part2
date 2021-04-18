b43042d97eb2bc2775a2653824ac786b514b56ab
PREMINDA <premindabandara0777@gmail.com>
2021-04-19 01:37:53 +0530

    Camera Follower

---
+++ Assets/Script/CamerController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamerController : MonoBehaviour
+{
+    private Transform playerTRG;
+
+    void Start()
+    {
+        playerTRG = PlayerController.instance.transform; 
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.position = new Vector3(playerTRG.position.x, playerTRG.position.y, transform.position.z);
+    }
+}
