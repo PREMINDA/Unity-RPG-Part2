4ae107518f5790b43b8fca9850baf04f716e84a1
PREMINDA <premindabandara0777@gmail.com>
2021-04-21 00:42:29 +0530

    Add dialog Activator

---
+++ Assets/Script/DialogActivatior.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogActivatior : MonoBehaviour
+{
+    private bool _isEnable=false;
+    private bool _isEscapeEnable = false;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.E) && _isEnable== true)
+        {
+            DialogManager.insance.setActivate(true);
+            PlayerController.instance.setcanwalk(0);
+            _isEnable = false;
+            _isEscapeEnable = true;
+        }
+        if (Input.GetKeyDown(KeyCode.Escape) && _isEscapeEnable == true)
+        {
+            DialogManager.insance.setActivate(false);
+            PlayerController.instance.setcanwalk(5f);
+            _isEnable = true;
+            _isEscapeEnable = false;
+        }
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Player")
+        {
+            _isEnable = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if(collision.tag == "Player")
+        {
+            _isEnable = false;
+            _isEnable = false;
+
+        }
+    }
+}
---
+++ Assets/Script/DialogManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogManager : MonoBehaviour
+{
+    public new Text name;
+    [SerializeField]
+    private Text dialog;
+    [SerializeField]
+    private GameObject diallogbox;
+    [SerializeField]
+    private GameObject namebox;
+
+    public string[] dialogs;
+    private int dnumber = 0;
+
+    public static DialogManager insance;
+
+
+    void Start()
+    {
+        insance = this;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void setActivate(bool set)
+    {
+        diallogbox.SetActive(set);
+    }
+}
