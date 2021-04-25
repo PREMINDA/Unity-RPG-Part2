15a862f98fdb20accd1414a12e54f94ab7b77940
PREMINDA <premindabandara0777@gmail.com>
2021-04-26 00:35:02 +0530

    Collected items bind with UI

---
+++ Assets/Script/ItemButton.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ItemButton : MonoBehaviour
+{
+    public Image buttonImage;
+    public Text amountText;
+    public int buttonValue;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
